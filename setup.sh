43d2fe274d7477f7135cebaad616c044c53c2fc3
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2018-06-22 09:48:43 +0200

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+[ "$(whoami)" != root ] && sudo "$O"
+
+DIR=$(cd $(dirname $0) && pwd)
+
+cp ${DIR}/update_ovh_dyndns /usr/local/bin/update_ovh_dyndns
+chmod +x /usr/local/bin/update_ovh_dyndns
+chmod og-w /usr/local/bin/update_ovh_dyndns
+ln -s /usr/local/bin/update_ovh_dyndns /etc/cron.hourly
+
+read -p "OVH User: " DYNDNS_USER
+read -s -p "OVH Password: " DYNDNS_PASSWORD
+echo ""
+read -p "OVH Domain Name: " DYNDNS_DOMAIN
+
+cat > toto << EOF
+USER=$DYNDNS_USER
+PASS=$DYNDNS_PASSWORD
+DOMAIN=$DYNDNS_DOMAIN
+EOF
+
+chmod 600 /etc/dyndns_ovh.conf
