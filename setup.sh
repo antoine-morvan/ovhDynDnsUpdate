93177cc506340e05f72ed0fad8642f230be6444f
Antoine Morvan <antoine.morvan.phd@gmail.com>
2024-11-05 22:24:32 +0100

    Add tool detect; use full paths; create dir

--- setup.sh
+++ setup.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
+DIGFOUND=false
+CURLFOUND=false
+if type -f dig &> /dev/null; then
+    DIGFOUND=true
+fi
+if type -f curl &> /dev/null; then
+    CURLFOUND=true
+fi
+
+if ! $DIGFOUND && ! $CURLFOUND; then
+    echo "Could not find neither dig nor curl; please install one"
+    exit 1
+fi
+
 [ "$(whoami)" != root ] && sudo "$0" && exit 0
 
 DIR=$(dirname $(readlink -f $BASH_SOURCE))
 
+mkdir -p /usr/local/bin/
 cp ${DIR}/update_ovh_dyndns /usr/local/bin/update_ovh_dyndns
 chmod +x /usr/local/bin/update_ovh_dyndns
 chmod og-w /usr/local/bin/update_ovh_dyndns
-ln -s /usr/local/bin/update_ovh_dyndns /etc/cron.hourly
+ln -s /usr/local/bin/update_ovh_dyndns /etc/cron.hourly/update_ovh_dyndns
 
 read -p "OVH User: " DYNDNS_USER
 read -s -p "OVH Password: " DYNDNS_PASSWORD
